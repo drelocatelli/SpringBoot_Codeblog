9319409753f885cbcd74954172fabaa13f4d2a50
Your Name <you@example.com>
2021-10-21 21:26:13 -0300

    adicionando pagina posts

--- src/main/java/com/spring/codeblog/controller/MainController.java
+++ src/main/java/com/spring/codeblog/controller/MainController.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.spring.codeblog.service.CodeblogService;
@@ -33,6 +35,15 @@ public class MainController {
 		model.setViewName("posts");
 		List<Post> posts = codeblogService.findAll();
 		model.addObject("posts", posts);
+		model.addObject("title", title);
+		return model;
+	}
+	
+	@RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+	public ModelAndView getPost(@PathVariable("id") long id) {
+		model.setViewName("post");
+		Post post = codeblogService.findById(id);
+		model.addObject("post", post);
 		model.addObject("title", title);
 		return model;
 	}
